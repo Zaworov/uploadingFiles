6f9b9c02ceeef48e918b66653e69624de7eaa552
Zawodniak <Piotr.Zawodniak@thomsonreuters.com>
2020-06-08 23:26:30 +0200

    Initial commit

---
+++ src/test/java/com/app/uploadingFiles/UploadingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.uploadingFiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadingFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
